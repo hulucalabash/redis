39ed1de0b482b9ca01b8f0f705bc0eb23d99d7b6
hulu <244959425@qq.com>
2021-07-15 15:14:14 +0800

    redisTest

--- src/test/java/git/redis/demo/RedisApplicationTests.java
+++ src/test/java/git/redis/demo/RedisApplicationTests.java
@@ -1,13 +1,21 @@
 package git.redis.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class RedisApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     void contextLoads() {
+
+
+        redisTemplate.opsForValue().set("mykey"," 关注狂神说公众号");
+        System.out.println(redisTemplate.opsForValue().get("mykey"));
     }
 
 }
